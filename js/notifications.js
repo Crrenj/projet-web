72ca3aa0d8423a0a14fc3d34ca0612f8278c035f
Sami0710 <150815639+Sami0710@users.noreply.github.com>
2023-12-10 16:14:45 +0100

    notification.js
    Inachevé

---
+++ js/notifications.js
@@ -0,0 +1,23 @@
+<script>
+    function supprimerContenu() {
+        var div = document.getElementById('event');
+
+        div.innerHTML = '';
+    }
+</script>
+
+<script>
+    function supprimerDemande() {
+        var div = document.getElementById('demande');
+
+        div.innerHTML = '';
+    }
+</script>
+
+<script>  function redirigerVersMessagerie() {
+    
+        var urlMessagerie = "messagerie.html";
+
+        window.location.href = urlMessagerie;
+    }
+</script>
