2b84b106ebe1c2ee2c18d8948a25bbc7c3a058d4
Piriyatharsini <151556852+Piriyatharsini@users.noreply.github.com>
2023-12-17 17:43:06 +0100

    Update supprimer_ami.php

---
+++ php/supprimer_ami.php
@@ -0,0 +1,31 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    if (isset($_POST['delete'])) {
+        $id = $_POST['id'];
+
+        $database = "Mon Réseau";
+        $db_handle = mysqli_connect('localhost', 'root', 'root', $database, 8889);
+
+        if ($db_handle) {
+            $db_found = mysqli_select_db($db_handle, $database);
+            if ($db_found) {
+                $sql = "DELETE FROM amis WHERE ID = '$id'";
+                $result = mysqli_query($db_handle, $sql);
+
+                if ($result) {
+                    echo "Ami supprimé avec succès.";
+                } else {
+                    echo "Erreur lors de la suppression: " . mysqli_error($db_handle);
+                }
+            } else {
+                echo "Database not found";
+            }
+            mysqli_close($db_handle);
+        } else {
+            echo "Erreur de connexion à MySQL: " . mysqli_connect_error();
+        }
+    }
+}
+
+header("Location: reseau.php"); // Redirigez vers la page réseau après la suppression
+?>
