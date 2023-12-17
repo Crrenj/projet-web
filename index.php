ecc04592eea86a9a84be744a054e105471a625a6
Crrenj <juanlantiguajorge@gmail.com>
2023-12-17 18:45:21 +0100

    new

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php 
+	session_start();
+
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	$user = $_SESSION['user'];
+	$admin = $_SESSION['admin'];
+?>
 <!DOCTYPE html>
 <head>
 	<meta charset="UTF-8">
@@ -425,5 +435,8 @@
 			</div>
 		</footer>
 	</div>
+	<?php 
+	echo "$user<br>$admin";
+	?>
 </body>
 </html>
