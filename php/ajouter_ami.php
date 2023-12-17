5b3c9a58998355c66ccf72ee787a7c194e347ec2
Piriyatharsini <151556852+Piriyatharsini@users.noreply.github.com>
2023-12-17 22:50:17 +0100

    Update ajouter_ami.php

--- php/ajouter_ami.php
+++ php/ajouter_ami.php
@@ -70,43 +70,32 @@
             <input type="text" class="form-control" placeholder="Rechercher une personne" name="search">
             <button type="submit">Rechercher</button>
         </form>
-        <select name="filter" onchange="this.form.submit()">
-            <option value="">Filtrer par...</option>
-            <option value="age">Nom</option>
-            <option value="sexe">Prénom</option>
-            <!-- Autres options de filtrage -->
-        </select>
     </nav>
     <main>
         <?php
-        $database = "Mon Réseau";
+        $database = "database";
         $db_handle = mysqli_connect('localhost', 'root', 'root', $database, 8889);
         $searchTerm = '';
         if ($db_handle) {
             $db_found = mysqli_select_db($db_handle, $database);
             $filter = isset($_GET['filter']) ? $_GET['filter'] : '';
 
-            $sql = "SELECT * FROM nouveau_amis WHERE Nom LIKE '%$searchTerm%' OR Prenom LIKE '%$searchTerm%'";
+            $sql = "SELECT * FROM Reation WHERE ID_Utilisateur1 LIKE '%$searchTerm%'";
 
-            if ($filter == "age") {
-                $sql .= " ORDER BY Age"; // Exemple, ajustez selon votre structure de base de données
-            } elseif ($filter == "sexe") {
-                $sql .= " ORDER BY Sexe"; // Exemple, ajustez selon votre structure de base de données
-            }
 
             if (isset($_GET['search'])) {
                 $searchTerm = mysqli_real_escape_string($db_handle, $_GET['search']);
-                $sql = "SELECT * FROM nouveau_amis WHERE Nom LIKE '%$searchTerm%' OR Prenom LIKE '%$searchTerm%'";
+                $sql = "SELECT * FROM Relation WHERE ID_Utilisateur1 LIKE '%$searchTerm%'";
                 $result = mysqli_query($db_handle, $sql);
                 if ($result) {
                     while ($person = mysqli_fetch_assoc($result)) {
                         echo "<div class='friend-card'>";
                         echo "<img src='" . $person['Photo'] . "' alt='Photo de " . $person['Prenom'] . "'>";
                         echo "<div>";
-                        echo "<p>" . $person['Nom'] . " " . $person['Prenom'] . "</p>";
-                        echo "<p>" . $person['Description'] . "</p>";
+                        echo "<p>" . $person['ID_Utilisateur1'] . "</p>";
+                        echo "<p>" . $person['Statut'] . "</p>";
                         echo "<form action='envoyer_demande_ami.php' method='post'>";
-                        echo "<input type='hidden' name='friend_id' value='" . $person['ID'] . "'>";
+                        echo "<input type='hidden' name='friend_id' value='" . $person['ID_Utilisateur1'] . "'>";
                         echo "<button type='submit' name='send_request'>Envoyer une demande</button>";
                         echo "</form>";
                         echo "</div>";
